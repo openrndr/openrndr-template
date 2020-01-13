0a478a98a6fff455094321d8abc1e07241484a8f
Edwin Jakobs <edwin@rndr.studio>
2020-01-13 21:00:56 +0100

    Fix orx-kinect-v1 native dependencies

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
-![0337_openrndr-3](https://user-images.githubusercontent.com/983309/70990617-39968280-20c6-11ea-8304-70ad0afa628d.png)
-# openrndr-template
+# OPENRNDR template project
 A feature rich template for creating OPENRNDR programs based on Gradle/Kts
-
-You will find some [basic instructions](https://guide.openrndr.org/#/02_Getting_Started_with_OPENRNDR/C00_SetupYourFirstProgram) in the [OPENRNDR guide](https://guide.openrndr.org)
 
 The template consists of a configuration for Gradle and an example OPENRNDR program. The Gradle configuration should serve as the
 go-to starting point for writing OPENRNDR-based software.
+
+If you are looking at this from IntelliJ IDEA you can start by expanding the _project_ tab on the left. You will find a template program in `src/main/kotlin/TemplateProgram.kt`
+
+You will find some [basic instructions](https://guide.openrndr.org/#/02_Getting_Started_with_OPENRNDR/C00_SetupYourFirstProgram) in the [OPENRNDR guide](https://guide.openrndr.org)
 
 ## Gradle tasks
  - `run` runs the TemplateProgram
--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ val orxUseSnapshot = false
 val orxVersion = if (orxUseSnapshot) "0.4.0-SNAPSHOT" else "0.3.44"
 
 // supported features are: orx-camera, orx-compositor,orx-easing, orx-filter-extension,orx-file-watcher, orx-fx
-// orx-integral-image, orx-interval-tree, orx-jumpflood, orx-kdtree, orx-mesh-generators,orx-midi, orx-no-clear,
+// orx-integral-image, orx-interval-tree, orx-jumpflood, orx-kinect-v1, orx-kdtree, orx-mesh-generators,orx-midi, orx-no-clear,
 // orx-noise, orx-obj, orx-olive, orx-osc, orx-palette, orx-runway
 val orxFeatures = setOf("orx-noise", "orx-fx")
 
@@ -65,54 +65,63 @@ fun DependencyHandler.openrndrNatives(module: String): Any {
     return "org.openrndr:openrndr-$module-natives-$openrndrOs:$openrndrVersion"
 }
 
+fun DependencyHandler.orxNatives(module: String): Any {
+    return "org.openrndr.extra:$module-natives-$openrndrOs:$orxVersion"
+}
+
+
 dependencies {
-    runtime(openrndr("gl3"))
-    runtime(openrndrNatives("gl3"))
-    compile(openrndr("openal"))
-    runtime(openrndrNatives("openal"))
-    compile(openrndr("core"))
-    compile(openrndr("svg"))
-    compile(openrndr("animatable"))
-    compile(openrndr("extensions"))
-    compile(openrndr("filter"))
+    runtimeOnly(openrndr("gl3"))
+    runtimeOnly(openrndrNatives("gl3"))
+    implementation(openrndr("openal"))
+    runtimeOnly(openrndrNatives("openal"))
+    implementation(openrndr("core"))
+    implementation(openrndr("svg"))
+    implementation(openrndr("animatable"))
+    implementation(openrndr("extensions"))
+    implementation(openrndr("filter"))
 
-    compile("org.jetbrains.kotlinx", "kotlinx-coroutines-core","1.3.3")
+    implementation("org.jetbrains.kotlinx", "kotlinx-coroutines-core","1.3.3")
 
-    compile("io.github.microutils", "kotlin-logging","1.7.8")
+    implementation("io.github.microutils", "kotlin-logging","1.7.8")
 
     when(applicationLogging) {
         Logging.NONE -> {
-            runtime("org.slf4j","slf4j-nop","1.7.29")
+            runtimeOnly("org.slf4j","slf4j-nop","1.7.29")
         }
         Logging.SIMPLE -> {
-            runtime("org.slf4j","slf4j-simple","1.7.29")
+            runtimeOnly("org.slf4j","slf4j-simple","1.7.29")
         }
         Logging.FULL -> {
-            runtime("org.apache.logging.log4j", "log4j-slf4j-impl", "2.13.0")
-            runtime("com.fasterxml.jackson.core", "jackson-databind", "2.10.1")
-            runtime("com.fasterxml.jackson.dataformat", "jackson-dataformat-yaml", "2.10.1")
+            runtimeOnly("org.apache.logging.log4j", "log4j-slf4j-impl", "2.13.0")
+            runtimeOnly("com.fasterxml.jackson.core", "jackson-databind", "2.10.1")
+            runtimeOnly("com.fasterxml.jackson.dataformat", "jackson-dataformat-yaml", "2.10.1")
         }
     }
 
     if ("video" in openrndrFeatures) {
-        compile(openrndr("ffmpeg"))
-        runtime(openrndrNatives("ffmpeg"))
+        implementation(openrndr("ffmpeg"))
+        runtimeOnly(openrndrNatives("ffmpeg"))
     }
 
     if ("panel" in openrndrFeatures) {
-        compile("org.openrndr.panel:openrndr-panel:$panelVersion")
+        implementation("org.openrndr.panel:openrndr-panel:$panelVersion")
     }
 
     for (feature in orxFeatures) {
-        compile(orx(feature))
+        implementation(orx(feature))
+    }
+
+    if ("orx-kinect-v1" in orxFeatures) {
+        runtimeOnly("orx-kinect-v1")
     }
 
     if ("orx-olive" in orxFeatures) {
-        compile("org.jetbrains.kotlin", "kotlin-scripting-compiler-embeddable")
+        implementation("org.jetbrains.kotlin", "kotlin-scripting-compiler-embeddable")
     }
 
     implementation(kotlin("stdlib-jdk8"))
-    testCompile("junit", "junit", "4.12")
+    testImplementation("junit", "junit", "4.12")
 }
 
 // --------------------------------------------------------------------------------------------------------------------
