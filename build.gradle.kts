b7dbf58574e9c483d4e2c7dd049cdae06f3b20c8
Edwin Jakobs <edwin@rndr.studio>
2020-07-06 19:18:53 +0200

    Bump to latest OPENRNDR release

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ import org.gradle.nativeplatform.platform.internal.DefaultNativePlatform
 
 /* the name of this project, default is the template version but you are free to change these */
 group = "org.openrndr.template"
-version = "0.3.12"
+version = "0.3.13"
 
 val applicationMainClass = "TemplateProgramKt"
 
@@ -58,10 +58,10 @@ val openrndrFeatures = setOf(
 
 /*  Which version of OPENRNDR and ORX should be used? */
 val openrndrUseSnapshot = false
-val openrndrVersion = if (openrndrUseSnapshot) "0.4.0-SNAPSHOT" else "0.3.43-rc.15"
+val openrndrVersion = if (openrndrUseSnapshot) "0.4.0-SNAPSHOT" else "0.3.43"
 
 val orxUseSnapshot = false
-val orxVersion = if (orxUseSnapshot) "0.4.0-SNAPSHOT" else "0.3.52-rc.6"
+val orxVersion = if (orxUseSnapshot) "0.4.0-SNAPSHOT" else "0.3.52"
 
 //<editor-fold desc="This is code for OPENRNDR, no need to edit this .. most of the times">
 val supportedPlatforms = setOf("windows", "macos", "linux-x64", "linux-arm64")
