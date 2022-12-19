63f10ce2bb780d1ed6854d00955529ea6986fa20
Edwin Jakobs <edwin@rndr.studio>
2022-12-19 21:27:47 +0100

    Upgrade to Kotlin 1.7.21, Jackson 2.14.1, log4j 2.19.0, jsoup 1.15.3, kotlin-csv 1.7.0

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,7 +8,7 @@ val ormlUseSnapshot = true
 dependencyResolutionManagement {
     versionCatalogs {
         create("libs") {
-            version("kotlin", "1.7.20")
+            version("kotlin", "1.7.21")
             version("openrndr", if (openrndrUseSnapshot) "0.5.1-SNAPSHOT" else "0.4.1")
             version("orx", if (orxUseSnapshot) "0.5.1-SNAPSHOT" else "0.4.1")
             version("orml", if (ormlUseSnapshot) "0.5.1-SNAPSHOT" else "0.4.1")
@@ -26,11 +26,11 @@ dependencyResolutionManagement {
             library("slf4j-nop", "org.slf4j", "slf4j-nop").versionRef("slf4j")
             library("slf4j-simple", "org.slf4j", "slf4j-simple").versionRef("slf4j")
 
-            version("jackson", "2.13.3")
+            version("jackson", "2.14.1")
             library("jackson-databind", "com.fasterxml.jackson.core", "jackson-databind").versionRef("jackson")
             library("jackson-json", "com.fasterxml.jackson.dataformat", "jackson-dataformat-yaml").versionRef("jackson")
 
-            version("log4j", "2.18.0")
+            version("log4j", "2.19.0")
             library("log4j-slf4j", "org.apache.logging.log4j", "log4j-slf4j-impl").versionRef("log4j")
 
             version("kotlinx-coroutines", "1.6.4")
@@ -43,9 +43,9 @@ dependencyResolutionManagement {
             library("kotlin-logging", "io.github.microutils:kotlin-logging-jvm:2.1.23")
             library("junit", "junit:junit:4.13.2")
 
-            library("jsoup", "org.jsoup:jsoup:1.15.2")
+            library("jsoup", "org.jsoup:jsoup:1.15.3")
             library("gson", "com.google.code.gson:gson:2.9.1")
-            library("csv", "com.github.doyaaaaaken:kotlin-csv-jvm:1.6.0")
+            library("csv", "com.github.doyaaaaaken:kotlin-csv-jvm:1.7.0")
         }
     }
 }
