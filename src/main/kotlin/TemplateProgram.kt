89aeaf68ebde688350a7d5d3fd5443191e18db5d
Abe Pazos <abe@hamoid.com>
2025-03-11 10:18:21 +0800

    Upgrade to openrndr/orx 0.4.5-alpha8

--- src/main/kotlin/TemplateProgram.kt
+++ src/main/kotlin/TemplateProgram.kt
@@ -2,7 +2,7 @@ import org.openrndr.application
 import org.openrndr.color.ColorRGBa
 import org.openrndr.draw.loadFont
 import org.openrndr.draw.loadImage
-import org.openrndr.draw.tint
+import org.openrndr.extra.color.colormatrix.tint
 import kotlin.math.cos
 import kotlin.math.sin
 
