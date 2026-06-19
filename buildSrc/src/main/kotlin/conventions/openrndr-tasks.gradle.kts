d780f5c11037bc9c71600a0ed013f97449cd794b
Abe Pazos <abe@hamoid.com>
2026-06-19 15:56:33 +0200

    Add descriptions to Gradle tasks
    Avoids warning.

--- buildSrc/src/main/kotlin/conventions/openrndr-tasks.gradle.kts
+++ buildSrc/src/main/kotlin/conventions/openrndr-tasks.gradle.kts
@@ -2,21 +2,26 @@ package conventions
 
 val applicationMainClass = providers.gradleProperty("applicationMainClass").get()
 tasks.register("create executable jar for $applicationMainClass") {
+    description = "Builds build/libs/openrndr-template-1.0.0-all.jar"
     group = " \uD83E\uDD8C OPENRNDR"
     dependsOn("shadowJar")
 }
 
 tasks.register("run $applicationMainClass") {
+    description = "Launches $applicationMainClass or the class specified via `-Popenrndr.application`"
     group = " \uD83E\uDD8C OPENRNDR"
     dependsOn("run")
 }
 
 tasks.register("create standalone executable for $applicationMainClass") {
+    description = "Creates build/distributions/openrndr-application.zip"
     group = " \uD83E\uDD8C OPENRNDR"
     dependsOn("jpackageZip")
 }
 
 tasks.register("add IDE file scopes") {
+    description = "Adds Code, Text, Gradle and Media IDE scopes." +
+            "Enable them to see less files in the project file browser."
     group = " \uD83E\uDD8C OPENRNDR"
     val scopesFolder = File("${project.projectDir}/.idea/scopes")
     scopesFolder.mkdirs()
