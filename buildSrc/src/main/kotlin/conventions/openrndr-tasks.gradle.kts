43e1156740db1a6fddc5c82069acebbc695af4d6
Abe Pazos <abe@hamoid.com>
2026-06-19 16:15:05 +0200

    Gradle `properties` deprecated, update

--- buildSrc/src/main/kotlin/conventions/openrndr-tasks.gradle.kts
+++ buildSrc/src/main/kotlin/conventions/openrndr-tasks.gradle.kts
@@ -1,6 +1,7 @@
 package conventions
 
-val applicationMainClass by properties
+val applicationMainClass = providers.gradleProperty("applicationMainClass").get()
+
 tasks.register("create executable jar for $applicationMainClass") {
     group = " \uD83E\uDD8C OPENRNDR"
     dependsOn("shadowJar")
